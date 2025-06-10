0536382fb9d9796dcf4a1b0c173a708ce1612ade
errerew <erlina31acc@gmail.com>
2025-06-10 18:52:01 +0700

    membetulkan bagian yang salh

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -6,8 +6,9 @@ using namespace std;
 int main(){
     cout << "Awal program" << endl;
     try {
-        array<int, 3> data = (10,20,30);
+        array<int, 3> data = {10, 20, 30,};
         cout << data.at(5)<<endl;
+        //memanggil array elemen ke 5
     }
     catch (exception& e) {
         //penangkap menggunakan obyek exception
@@ -15,5 +16,7 @@ int main(){
         //akan dieksekusi karna array data hanya memiliki3 elemen
     }
     cout << "Baris Progrm yang terakhir" << endl;
+    //penanda 2/ bahwa program berjalan tanpa henti
+    //meskipun terjadi kesalahan/
     return 0;
 }
