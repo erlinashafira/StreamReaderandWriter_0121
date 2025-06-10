39330e19cbe1a9755479c9e6e7aff6d56e4807b4
errerew <erlina31acc@gmail.com>
2025-06-10 18:28:08 +0700

    menambahkan perintah untuk kasus jika file ada maka melakukan perulangan setiap baris, jika tidak maka unable

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -25,5 +25,19 @@ int main(){
     }
     //selesai dalam menulis sekarang tutup filenya
     outfile.close();
-    
+    //membuka file dalam mode membaca
+    ifstream infile;
+    infile.open(NamaFile, ios::in);
+    cout << endl << ">= Membuka dan membaca file " << endl;
+
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+        infile.close();
+    }
+    else cout << "Unable to open file";
+    return 0;
 }
