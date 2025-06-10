c239ebaf94ebbcd585de07959c618d7226407a28
errerew <erlina31acc@gmail.com>
2025-06-10 18:39:56 +0700

    menambahkan perintah try catch

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -6,6 +6,14 @@ using namespace std;
 int main(){
     cout << "Awal program" << endl;
     try {
-        
+        array<int, 3> data = (10,20,30);
+        cout << data.at(5)<<endl;
     }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        //akan dieksekusi karna array data hanya memiliki3 elemen
+    }
+    cout << "Baris Progrm yang terakhir" << endl;
+    return 0;
 }
