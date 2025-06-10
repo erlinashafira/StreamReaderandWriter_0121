ecc6529325ede5592368755c099549320a9c74a8
errerew <erlina31acc@gmail.com>
2025-06-10 18:19:36 +0700

    menambahkan perintah untuk menutup file jika sudah selesai

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -13,11 +13,17 @@ int main(){
     //menunjuk ke sebuah nama file
     outfile.open(NamaFile + ".txt", ios::out);
     cout << ">= Menulis file \'q\' untuk keluar" << endl;
-
+    //unlimited loop untuk menulis
     while (true){
         cout << "- ";
+        //mendapatkan setiap karakter dlaam satu baris
         getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
         if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dlaam file
         outfile << baris << endl;
     }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+    
 }
