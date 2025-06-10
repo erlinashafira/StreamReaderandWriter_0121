9378c285e1cae1a4d007b80fe44f8f6de6886028
errerew <erlina31acc@gmail.com>
2025-06-10 18:50:37 +0700

    menambahkan catch.., jika selain integer maka block ini akan dieksekusi

--- manualException.cpp
+++ manualException.cpp
@@ -4,10 +4,16 @@ using namespace std;
 int main(){
     try {
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
-        throw 0,5;
+        throw 0,5; //melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan di eksekusi" << endl;
     }
     catch (int a) {
+        //blok ini akan dieksekusi
         cout << "Pengecualian akan dieksekusi" << endl;
     }
+    catch (...) {
+        //jika selain integer maka block ini akan di eksekusi
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+    return 0;
 }
