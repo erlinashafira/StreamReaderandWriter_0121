8f9697f7de6eb88799b080496df9405f4e9f12ba
errerew <erlina31acc@gmail.com>
2025-06-10 17:29:46 +0700

    menambahkan program untuk membuka data file dalam mode menulis

--- iofile.cpp
+++ iofile.cpp
@@ -5,4 +5,10 @@ using namespace std;
 
 int main (){
     string baris;
+    ofstream outfile;
+    outfile.open("contohfile.txt");
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    while (true) {
+        
+    }
 }
